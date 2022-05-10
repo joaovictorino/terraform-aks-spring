b6cef5fb5bde6ed80f95e3e3dca1522cc7785720
joaovictorino <jhvictorino@gmail.com>
2022-05-10 15:35:04 -0300

    correção deploy

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,14 @@
-# login azure (primeira execução)
-az login
-
-# criar chaves de acesso ao Azure para o terraform (primeira execução)
-az ad sp create-for-rbac --skip-assignment
+#!/bin/bash
 
 cd terraform
 
 # iniciar terraform (primeira execução)
 terraform init
 
-# planejar alterações
-terraform plan
-
 # alterar ambiente
-terraform apply
+terraform apply -auto-approve
+
+cd ..
 
 # compilar imagem
 docker build -t springapp .
@@ -28,7 +23,7 @@ az acr login --name aulainfraacrk
 docker push aulainfraacrk.azurecr.io/springapp:latest
 
 # obter credenciais do AKS
-az aks get-credentials --resource-group rg-aulainfra --name teste-aks
+az aks get-credentials --resource-group rg-aulainfra --name teste-aks  --overwrite-existing
 
 # subir configuração da aplicação
 kubectl apply -f aks/1-config
@@ -39,4 +34,14 @@ kubectl apply -f aks/2-db
 # subir configuração da aplicação
 kubectl apply -f aks/3-app
 
-URL http://springpetapp.eastus.cloudapp.azure.com/
+# instalar EFK
+kubectl apply -f efk/01-namespace.yaml
+kubectl apply -f efk/02-elastic-svc.yaml
+kubectl apply -f efk/03-elastic-stateful.yaml
+kubectl apply -f efk/04-fluentd-security.yaml
+kubectl apply -f efk/05-fluentd-daemon.yaml
+kubectl apply -f efk/06-kibana-svc.yaml
+kubectl apply -f efk/07-kibana-deployment.yaml
+
+# kubectl port-forward --namespace kube-logging svc/kibana 5601:5601
+# curl http://springpetapp.eastus.cloudapp.azure.com/
