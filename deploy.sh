e3c3c06cdfdac94d85c8dc71ca02a90b21d6517e
joaovictorino <jhvictorino@gmail.com>
2024-02-11 17:25:05 -0300

    EKS

--- deploy.sh
+++ deploy.sh
@@ -26,15 +26,15 @@ docker push springinfraacrk.azurecr.io/springapp:latest
 az aks get-credentials --resource-group rg-springinfra --name teste-aks --overwrite-existing
 
 # instalar Elastic Stack
-kubectl apply -f elastic/01-namespace.yaml
-kubectl apply -f elastic/02-elastic.yaml
+# kubectl apply -f elastic/01-namespace.yaml
+# kubectl apply -f elastic/02-elastic.yaml
 
-sleep 180
+# sleep 180
 
-kubectl apply -f elastic/03-kibana.yaml
-kubectl apply -f elastic/04-filebeat.yaml
+# kubectl apply -f elastic/03-kibana.yaml
+# kubectl apply -f elastic/04-filebeat.yaml
 
-sleep 60
+# sleep 60
 
 # subir configuração da aplicação
 kubectl apply -f aks/1-config
